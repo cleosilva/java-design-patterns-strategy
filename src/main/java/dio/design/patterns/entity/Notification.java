4ce0ec5166dcdf65573df8816d2d53ab5b588e8d
Cleo Silva <cleo_silva1906@hotmail.com>
2024-11-06 14:15:31 -0300

    Implemented Entities

---
+++ src/main/java/dio/design/patterns/entity/Notification.java
@@ -0,0 +1,35 @@
+package dio.design.patterns.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+
+    public Notification(Long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
